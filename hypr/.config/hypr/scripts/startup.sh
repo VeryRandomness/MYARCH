9f7b651054fc44af228a5bb0d31fdbf37eb2e2a7
VeryRandomness <christopher.j.butters@gmail.com>
2025-10-03 11:08:50 -0600

    Update startup.sh

--- hypr/.config/hypr/scripts/startup.sh
+++ hypr/.config/hypr/scripts/startup.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+/usr/lib/lxsession/main/lxpolkit &
+
+# Start Waybar, SwayNC, and other services in the background
+waybar &
+hyprpaper &
+hypridle &
+hyprshot &
+
+# Give swaync a moment to start up and listen for notifications
+sleep 2
+swaync &
+
+# Start other services
